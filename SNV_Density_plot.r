011c08fa992e3583e39a4214ce5c440ac75d70d2
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2022-12-09 20:25:57 +0800

    Update SNV_Density_plot.r

--- SNV_Density_plot.r
+++ SNV_Density_plot.r
@@ -1,4 +1,6 @@
 #!/public/store5/DNA/Test/zhengfuxing/conda/bin/Rscript
+# conda activate /public/store5/DNA/Test/zhengfuxing/conda
+# Rscript plot.r -o ./output -i indel.annotation.xls -s snp.annotation.xls
 library("optparse")
 # 传参
 option_list <- list(
@@ -9,15 +11,15 @@ option_list <- list(
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
+all_args <- names(opt)
 df_indel <- opt$i
 df_snp <- opt$s
 output <- opt$o
-
 ### 判断参数是否合格
-if(!opt$o){
+if(!"output" %in% all_args){
   print("请输入参数 -o 指定输出目录")
   q()
-}else if(!opt$s & !opt$i){
+}else if(!"indel" %in% all_args & !"snp" %in% all_args){
   print("-i -s 至少存在一个！")
   q()
 }
@@ -27,10 +29,10 @@ library("CMplot")
 
 list <- c()
 
-if(exists("opt$i")){
+if("indel" %in% all_args){
   list <- c(list,"indel")
 }
-if(exists("opt$s")){
+if("snp" %in% all_args){
   list <- c(list,"snp")
 }
 
@@ -55,27 +57,38 @@ for(type in list){  # 这里必须是先 indel 再 snp！！！！
     data <- data.frame(data[,1:3],data[sample_col])
     colnames(data)[4] <- colnames(df[sample_col])
     Main <- paste("The number of ",TYPE," within 1 Mb window size",sep = "")
-    CMplot(data,
-           type="p",
+    sample <- sub("\\.",'-',colnames(data)[4])
+    outfile = paste(output,"/",TYPE,"_Density_",sample,sep="")
+    # 需要生成pdf 可以直接取消注释
+    # pdf(file=paste(outfile,".pdf",sep=""),width = 9,height = 6)
+    # CMplot(data,
+    #        type="p",
+    #        plot.type="d",
+    #        bin.size=1e6,
+    #        chr.den.col=c("grey", "black"),
+    #        file="pdf",
+    #        memo="",  # 输出文件名添加内容
+    #        dpi=450,
+    #        file.output=F,
+    #        verbose=TRUE,
+    #        width=9,
+    #        height=6,
+    #        main = Main)  # 修改标题
+    # dev.off()
+    
+    png(file=paste(outfile,".png",sep=""),width = 900,height = 600)
+    CMplot(data,type="p",
            plot.type="d",
            bin.size=1e6,
            chr.den.col=c("grey", "black"),
            file="pdf",
            memo="",  # 输出文件名添加内容
            dpi=450,
-           file.output=TRUE,
+           file.output=F,
            verbose=TRUE,
            width=9,
            height=6,
            main = Main)  # 修改标题
-  }
-
-  results <- list.files(pattern = "SNP")
-  for(file in results){
-    newname <- sub("SNP-Density\\.",'',file)
-    newname <- sub("\\.pdf",'',newname)
-    newname <- sub("\\.",'-',newname)
-    newname <- paste(TYPE,"-Density.",newname, ".pdf",sep = "")
-    file.rename(file,newname)
+    dev.off()
   }
 }
