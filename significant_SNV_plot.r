8e901d35b693b22f966a07e8189d0b459f8797ef
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-02-23 16:20:24 +0800

    封装 + 外界传参

--- significant_SNV_plot.r
+++ significant_SNV_plot.r
@@ -1,20 +1,80 @@
+#!/public/store5/DNA/Test/zhengfuxing/conda/bin/Rscript
+# conda activate /public/store5/DNA/Test/zhengfuxing/conda
 rm(list=ls())
+.libPaths(c("/public/store5/DNA/Test/zhengfuxing/conda/lib/R/library","/data/software/R_package/DNA/3.5"))
+# 传参
+library(optparse)
 
-threshold = 10**(-7.83)
-prefix = 'feral'
-wkdir = '/public/store5/DNA/Project/Evolution/2021/HT2021-24625_Ramie_population/GEA/population_reseq_feral/population_reseq/HT2021-24625-吴增源-GWAS项目结题报告/5.GWAS/Annotation/'
-samples = c()
-for(i in dir(wkdir, pattern = "xls")){a=sub("_.+","",i);samples = append(samples,a)}
-samples[1] = 'altitude'
+option_list <- list(
+  make_option(c("-d","--dir"),type="character",help="输入文件所在目录：GWAS 位点显著性统计所在目录，非 filter！"),
+  make_option(c("-g","--genome_sizes"),type="character",help="参考基因组对应的 genome.fa.sizes.chrom ！"),
+  make_option(c("-t","--threshold"),type="double",default=8,help="显著性阈值 取 log10(t)"),
+  make_option(c("-b","--backColor"),type="character", default="#CCCCCC",help="背景染色体颜色，默认灰色，支持16进制颜色"),
+  make_option(c("-s","--snpColor"),type="character", default="black", help="显著性位点颜色，默认黑色，支持16进制颜色"),
+  make_option(c("-o","--output"),type="character", default="./",help="文件是输出路径"),
+  make_option(c("-p","--phenotype_sort"),type="character", default="nosort",help="表型展示顺序排序，从上向下排序 使用逗号分隔，eg: high,weight")
+
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+
+
+
+
+
+
+# 脚本参数赋值
+threshold = 10**-(opt$t)  #  阈值
+out = opt$o
+wkdir = opt$d  #  表型显著性结果所在目录
+
+genome_fa_sizes_chrom = opt$g  #  基因组对应 genome.fa.sizes.chrom 路径
+outfile <- paste(out,'/','Significant','_snp_plot',sep = "")
+#bkcolor <- "#CCCCCC" 默认
+bkcolor <- opt$b
+snpcolor <- opt$s
+Sort <-  opt$p
+##################################################################
+# 参数处理
+# 为防止目录输入错误
+if(grepl("/$",wkdir)){
+  print('处理文件时间较长，请等待......')
+}else if(grepl("\\\\$",wkdir)){
+  print('处理文件时间较长，请等待......')
+}else{
+  print('处理文件时间较长，请等待......')
+  wkdir <- paste(wkdir,'/',sep = '')
+}
+# 手动排序表型文件
+# 如果不指定表型顺序
+if(Sort == 'nosort'){
+  files <- dir(wkdir,pattern = "*xls")
+}else{  # 如果指定表型顺序
+  temp <- strsplit(Sort,',')[[1]]
+  files = c()
+  for(i in temp)
+    files = c(files,paste(i,"_GWAS.anno.xls",sep = ''))
+}
+
+
+##################################################################
+
+phenotype = c()
+for(i in files){a=sub("_GWAS.anno.xls","",i);phenotype = append(phenotype,a)}
+
+len <- length(phenotype)
+
 
 # 处理背景
-ref <- read.table('/public/store5/DNA/Test/zhangchunyuan/WuZengyuan/HT2021-24625_Ramie_population/population_analysis/ref/genome.fa.sizes.chrom')
+ref <- read.table(genome_fa_sizes_chrom)
 colnames(ref) <- c('chr', 'size')
 ref$size <- ref$size/10000000
 
 size2 <- c()
 counts <- 0
 space <- 0.2
+
 for(i in ref$size){
   if(counts==0){
     size2 <- c(size2,i)
@@ -29,32 +89,34 @@ ref$xl <- size3
 ref$xr <- size2
 
 
-outfile <- paste('./',prefix,'_SNV_plot',sep = "")
+
+
+# 确定 X 轴 最大坐标
+mx = ceiling(max(ref$xr))
 
 ########################################
 #画背景
-sample <- c('altitude','bio1','bio2','bio3','bio4','bio5','bio6','bio7','bio8','bio9','bio10','bio11','bio12','bio13','bio14','bio15','bio16','bio17','bio18','bio19')
-len <- length(sample)
-files <- c('altS','bio1_29','bio2_29','bio3_29','bio4_29','bio5_29','bio6_29','bio7_29','bio8_29','bio9_29','bio10_29','bio11_29','bio12_29','bio13_29','bio14_29','bio15_29','bio16_29','bio17_29','bio18_29','bio19_29')
+
 pdf(file=paste(outfile,".pdf",sep=""),width = 9,height = 6)
 a<-dev.cur()
 png(file=paste(outfile,".png",sep=""),width = 900,height = 600)
 dev.control("enable")
-par(mar=c(1,2,1,4))
-plot(1:len,1:len,type="n",ylim=c(0,len+1),xlim=c(0,29),bty="n",yaxt="n",xaxt="n",xlab="Chromosome",ylab="",xaxs="i") # 打开画布
-axis(4,at=(c(1:len)-0.5),labels=rev(sample),las=2,col = "NA", col.ticks = "NA") # Y轴标签
-for(k in (1:length(sample))){
+par(mar=c(1,2,1,6))
+plot(1:len,1:len,type="n",ylim=c(0,len+1),xlim=c(0,mx),bty="n",yaxt="n",xaxt="n",xlab="Chromosome",ylab="",xaxs="i") # 打开画布
+axis(4,at=(c(1:len)-0.5),labels=rev(phenotype),las=2,col = "NA", col.ticks = "NA") # Y轴标签
+# 开始处理文件
+for(k in (1:length(phenotype))){
   y_b <- k-0.8
   y_t <- k-0.2
   #画背景
   for(j in (1:dim(ref)[1])){
     x_l <- ref$xl[j]
     x_r <- ref$xr[j]
-    rect(x_l,y_b,x_r,y_t,border = "#CCCCCC",angle = 30,lwd = 0, col = "#CCCCCC") # 矩形绘制
+    rect(x_l,y_b,x_r,y_t,border = bkcolor,angle = 30,lwd = 0, col = bkcolor) # 矩形绘制
   }
 
   # 处理显著位点
-  file <- paste(wkdir,rev(files)[k],"_GWAS.anno.xls",sep = "")
+  file <- paste(wkdir,rev(files)[k],sep = '')
   print(file)
   df <- read.table(file,header = T);
   df <- df[,c(1,2,5)]
@@ -80,7 +142,7 @@ for(k in (1:length(sample))){
       x_l <- df$pxl[j]
       x_r <- df$pxr[j]
 
-      lines(x=c(x_l,x_l),y=c(y_b,y_t)) # 显著位点绘制
+      lines(x=c(x_l,x_l),y=c(y_b,y_t),col = snpcolor) # 显著位点绘制
     }
   }
 
