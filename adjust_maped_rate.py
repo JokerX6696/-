c4fb89e409d96f09ca6d8847c67a30c93a232394
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-03-22 11:01:25 +0800

    Update adjust_maped_rate.py

--- adjust_maped_rate.py
+++ adjust_maped_rate.py
@@ -1,8 +1,8 @@
 #!/public/store5/DNA/Test/zhengfuxing/conda/bin/python
 import os, re
 #### 参数
-Threshold = 0.8
-
+Threshold_jl = 0.95
+Threshold_t = 0.8
 ###
 samples = ['CB-3',
         'F1-1',
@@ -13,9 +13,9 @@ samples = ['CB-3',
         'F3-3',
         'NCRC-F1-1'
 ]
-file_dir = '/public/store5/DNA/Project/Reseq/2022/DOE202213215-b1/PA/30_percent/stat/PA20230308/DOE202213215-王石-数据高级分析售后-20230301/'
+file_dir = '/public/store5/DNA/Project/Reseq/2022/DOE202213215-b1/PA/30_percent/stat/PA20230322/DOE202213215-王石-数据高级分析售后-20230301/'
 for sample in samples:
-    outfile = open(sample + '_stat.xls','w')
+    outfile = open(sample + '_' + str(Threshold_t) + '_stat.xls','w')
     # 统计上一次结果 用于后续修正
     with open('/public/store5/DNA/Project/Reseq/2022/DOE202213215-b1/PA/30_percent/stat/reads_name/' + sample + '_stat.txt', 'r')as f:
         common = int(f.readline().split(' ')[-1])
@@ -37,28 +37,34 @@ for sample in samples:
             common -= 1
             continue
         jiyu, liyu, tuantoufang = line.split('\t')[1:4]
-        if float(jiyu) < Threshold and float(liyu) < Threshold and float(tuantoufang) < Threshold:
-            # 如果三个物种均低于阈值，则 共有 -1
+        if float(jiyu) < Threshold_jl and float(liyu) < Threshold_jl and float(tuantoufang) < Threshold_t:
+            # 如果三个物种均低于阈值，则最高的特有
             common -= 1
-        elif float(jiyu) >= Threshold and float(liyu) >= Threshold and float(tuantoufang) >= Threshold:
+            if max([jiyu, liyu, tuantoufang]) == jiyu:
+                j += 1
+            elif max([jiyu, liyu, tuantoufang]) == tuantoufang:
+                t += 1
+            elif max([jiyu, liyu, tuantoufang]) == liyu:
+                l += 1
+        elif float(jiyu) >= Threshold_jl and float(liyu) >= Threshold_jl and float(tuantoufang) >= Threshold_t:
             continue
         else:
-            if float(jiyu) < Threshold and float(liyu) < Threshold:
+            if float(jiyu) < Threshold_jl and float(liyu) < Threshold_jl:
                 common -= 1
                 t += 1
-            elif float(jiyu) >= Threshold and float(liyu) >= Threshold:
+            elif float(jiyu) >= Threshold_jl and float(liyu) >= Threshold_jl:
                 common -= 1
                 l_j += 1
-            elif float(jiyu) < Threshold and float(tuantoufang) < Threshold:
+            elif float(jiyu) < Threshold_jl and float(tuantoufang) < Threshold_t:
                 common -= 1
                 l += 1
-            elif float(jiyu) >= Threshold and float(tuantoufang) >= Threshold:
+            elif float(jiyu) >= Threshold_jl and float(tuantoufang) >= Threshold_t:
                 common -= 1
                 j_t += 1
-            elif float(liyu) < Threshold and float(tuantoufang) < Threshold:
+            elif float(liyu) < Threshold_jl and float(tuantoufang) < Threshold_t:
                 common -= 1
                 j += 1
-            elif float(liyu) >= Threshold and float(tuantoufang) >= Threshold:
+            elif float(liyu) >= Threshold_jl and float(tuantoufang) >= Threshold_t:
                 common -= 1
                 l_t += 1
             else:
@@ -77,16 +83,20 @@ for sample in samples:
             l_j -= 1
             continue
         jiyu, liyu = line.split('\t')[1:3]
-        if float(jiyu) < Threshold and float(liyu) < Threshold:
+        if float(jiyu) < Threshold_jl and float(liyu) < Threshold_jl:
             # 如果2个物种均低于阈值，则 共有 -1
             l_j -= 1
-        elif float(jiyu) >= Threshold and float(liyu) >= Threshold:
+            if max([jiyu, liyu]) == jiyu:
+                j += 1
+            elif max([jiyu, liyu]) == liyu:
+                l += 1
+        elif float(jiyu) >= Threshold_jl and float(liyu) >= Threshold_jl:
             continue
         else:
-            if float(jiyu) < Threshold:
+            if float(jiyu) < Threshold_jl:
                 l_j -= 1
                 l += 1
-            elif float(liyu) < Threshold:
+            elif float(liyu) < Threshold_jl:
                 l_j -= 1
                 j += 1
             else:
@@ -108,16 +118,20 @@ for sample in samples:
             j_t -= 1
             continue
         jiyu, tuantoufang = line.split('\t')[1:3]
-        if float(jiyu) < Threshold and float(tuantoufang) < Threshold:
+        if float(jiyu) < Threshold_jl and float(tuantoufang) < Threshold_t:
             # 如果2个物种均低于阈值，则 共有 -1
             j_t -= 1
-        elif float(jiyu) >= Threshold and float(tuantoufang) >= Threshold:
+            if max([jiyu, tuantoufang]) == jiyu:
+                j += 1
+            elif max([jiyu, tuantoufang]) == tuantoufang:
+                t += 1
+        elif float(jiyu) >= Threshold_jl and float(tuantoufang) >= Threshold_t:
             continue
         else:
-            if float(jiyu) < Threshold:
+            if float(jiyu) < Threshold_jl:
                 j_t -= 1
                 t += 1
-            elif float(tuantoufang) < Threshold:
+            elif float(tuantoufang) < Threshold_t:
                 j_t -= 1
                 j += 1
             else:
@@ -125,7 +139,7 @@ for sample in samples:
                 print(line)
                 exit(812)
     # 鲤鱼 & 团头鲂
-    with open(file_dir + sample + '_j_t_readsname_result.xls')as f:
+    with open(file_dir + sample + '_l_t_readsname_result.xls')as f:
         lines = [i.replace('\n', '') for i in f.readlines()]
 
     for line in lines:
@@ -135,16 +149,20 @@ for sample in samples:
             l_t -= 1
             continue
         liyu, tuantoufang = line.split('\t')[1:3]
-        if float(liyu) < Threshold and float(tuantoufang) < Threshold:
+        if float(liyu) < Threshold_jl and float(tuantoufang) < Threshold_t:
             # 如果2个物种均低于阈值，则 共有 -1
             l_t -= 1
-        elif float(liyu) >= Threshold and float(tuantoufang) >= Threshold:
+            if max([liyu, tuantoufang]) == liyu:
+                l += 1
+            elif max([liyu, tuantoufang]) == tuantoufang:
+                t += 1
+        elif float(liyu) >= Threshold_jl and float(tuantoufang) >= Threshold_t:
             continue
         else:
-            if float(liyu) < Threshold:
+            if float(liyu) < Threshold_jl:
                 l_t -= 1
                 t += 1
-            elif float(tuantoufang) < Threshold:
+            elif float(tuantoufang) < Threshold_t:
                 l_t -= 1
                 l += 1
             else:
