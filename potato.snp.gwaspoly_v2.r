0836ab0c4de3cf0a0e9a62e83c4fc50285161042
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-04-11 18:06:33 +0800

    马铃薯四倍体 GWAS

--- potato.snp.gwaspoly_v2.r
+++ potato.snp.gwaspoly_v2.r
@@ -1,22 +1,17 @@
-#!/home/fanyucai/software/R/R-v3.2.0/bin/Rscript
-#================================================
-#
-#         Author: baozhigui
-#          Email: zhigui.bao@oebiotech.com
-#         Create: 2018-09-27 09:12:13
-#    Description: -
-#
-#================================================
-rm(list=ls())
+#!/public/store5/DNA/Test/zhengfuxing/conda/bin/Rscript
+# conda activate /public/store5/DNA/Test/zhengfuxing/conda
+.libPaths(c("/public/store5/DNA/Test/zhengfuxing/conda/lib/R/library","/data/software/R_package/DNA/3.5"))
 library(GWASpoly)
 library(data.table)
 library(ggplot2)
 # load datat
 # format can be ACGT or numeric
-setwd('D:/desk/马铃薯_夏露露/')
-#source("D:/desk/马铃薯_夏露露/read.GWASpoly.R")
+###########################
+#traits_num = 27
 
-data <- read.GWASpoly(ploidy=4,pheno.file="15trait.txt",geno.file="extract_snp.130samples.tetraploid.genotype",format="ACGT",n.traits=15,delim="\t")
+
+###########################
+data <- read.GWASpoly(ploidy=4,pheno.file="27BLUE.txt",geno.file="extract_snp.130samples.tetraploid.genotype",format="ACGT",n.traits=27,delim="\t")
 
 # kinship
 data2 <- set.K(data, LOCO = TRUE)
@@ -25,32 +20,34 @@ data2 <- set.K(data, LOCO = TRUE)
 params <- set.params(fixed=c("Grp1","Grp2","Grp3","Grp4"),fixed.type=rep("numeric",4),n.PC=4)
 
 trait_name <- as.character(colnames(data@pheno))
-trait_name <- trait_name[2:16]
+trait_name <- trait_name[2:28]
 # Mixed model analysis
 # data3 <- GWASpoly(data2,models=c("general","additive","1-dom"),traits=trait_name,n.core=15)
-data3 <- GWASpoly(data2,models=c("general","additive"),traits=trait_name,n.core=15)
+data3 <- GWASpoly(data2,models=c("general","additive","1-dom","2-dom"),traits=trait_name,n.core=15)
 # model qq plot
-models <- c("additive","general")
-for (i in 1:15){
-  for (j in 1:2){
-  p <- qq.plot(data3,trait=trait_name[i],model=models[j])
-  ggsave(filename = paste(trait_name[i],models[j],"qqplot.pdf",sep="_"),plot = p,height=8,width=10,device = 'pdf')
-  } 
+models <- c("additive","general",'1-dom-alt','1-dom-ref','2-dom-alt','2-dom-ref')
+for (i in 1:27){
+  for (j in 1:6){
+    p <- qq.plot(data3,trait=trait_name[i],model=models[j])
+    ggsave(filename = paste(trait_name[i],models[j],"qqplot.pdf",sep="_"),plot = p,height=8,width=10,device = 'pdf')
+    ggsave(filename = paste(trait_name[i],models[j],"qqplot.png",sep="_"),plot = p,height=8,width=10,device = 'png')
+  }
 }
 
 #manhattan.plot(data4,trait="Long_width",model="additive")
 # get result
 data4 <- set.threshold(data3,method="Bonferroni",level=0.05)
 
-for (i in 1:15){
-  for (j in 1:2){
+for (i in 1:27){
+  for (j in 1:6){
     p <- manhattan.plot(data4,trait=trait_name[i],model=models[j])
     ggsave(filename = paste(trait_name[i],models[j],"manhattan.pdf",sep="_"),plot = p,height=8,width=10,device = 'pdf')
+    ggsave(filename = paste(trait_name[i],models[j],"manhattan.png",sep="_"),plot = p,height=8,width=10,device = 'png')
   }
 }
 
-for (i in 1:15){
+for (i in 1:27){
   write.GWASpoly(data4,trait_name[i], paste(trait_name[i],"_scores.xls",sep=""), what = "scores", delim = "\t")
   write.GWASpoly(data4,trait_name[i], paste(trait_name[i],"_effects.xls",sep=""), what = "effects", delim = "\t")
 }
-#write.GWASpoly(data3, "long_width", "tuber_shape_data3.effects.xls", what = "effects", delim = "\t")
+
