d59993aa166516b2412489096b26beb0ec93850e
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-05-10 09:04:24 +0800

    马铃薯四倍体参数标准化！

--- potato.snp.gwaspoly_v2.r
+++ potato.snp.gwaspoly_v2.r
@@ -7,11 +7,12 @@ library(ggplot2)
 # load datat
 # format can be ACGT or numeric
 ###########################
-#traits_num = 27
-
-
+# param
+trait_file = 'clean_trait_data.txt'
+traits_num = 6
+pos_file = 'extract_snp.130samples.tetraploid.genotype'
 ###########################
-data <- read.GWASpoly(ploidy=4,pheno.file="27BLUE.txt",geno.file="extract_snp.130samples.tetraploid.genotype",format="ACGT",n.traits=27,delim="\t")
+data <- read.GWASpoly(ploidy=4,pheno.file=trait_file,geno.file=pos_file,format="ACGT",n.traits=traits_num7,delim="\t")
 
 # kinship
 data2 <- set.K(data, LOCO = TRUE)
@@ -20,13 +21,13 @@ data2 <- set.K(data, LOCO = TRUE)
 params <- set.params(fixed=c("Grp1","Grp2","Grp3","Grp4"),fixed.type=rep("numeric",4),n.PC=4)
 
 trait_name <- as.character(colnames(data@pheno))
-trait_name <- trait_name[2:28]
+trait_name <- trait_name[2:traits_num]
 # Mixed model analysis
 # data3 <- GWASpoly(data2,models=c("general","additive","1-dom"),traits=trait_name,n.core=15)
 data3 <- GWASpoly(data2,models=c("general","additive","1-dom","2-dom"),traits=trait_name,n.core=15)
 # model qq plot
 models <- c("additive","general",'1-dom-alt','1-dom-ref','2-dom-alt','2-dom-ref')
-for (i in 1:27){
+for (i in 1:traits_num){
   for (j in 1:6){
     p <- qq.plot(data3,trait=trait_name[i],model=models[j])
     ggsave(filename = paste(trait_name[i],models[j],"qqplot.pdf",sep="_"),plot = p,height=8,width=10,device = 'pdf')
@@ -38,7 +39,7 @@ for (i in 1:27){
 # get result
 data4 <- set.threshold(data3,method="Bonferroni",level=0.05)
 
-for (i in 1:27){
+for (i in 1:traits_num){
   for (j in 1:6){
     p <- manhattan.plot(data4,trait=trait_name[i],model=models[j])
     ggsave(filename = paste(trait_name[i],models[j],"manhattan.pdf",sep="_"),plot = p,height=8,width=10,device = 'pdf')
@@ -46,8 +47,7 @@ for (i in 1:27){
   }
 }
 
-for (i in 1:27){
+for (i in 1:traits_num){
   write.GWASpoly(data4,trait_name[i], paste(trait_name[i],"_scores.xls",sep=""), what = "scores", delim = "\t")
   write.GWASpoly(data4,trait_name[i], paste(trait_name[i],"_effects.xls",sep=""), what = "effects", delim = "\t")
 }
-
