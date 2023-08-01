01e72bcd6bb71acaea67e43bf023bb22649fdf54
JokerX6696 <313445042@qq.com>
2023-08-01 16:46:17 +0800

    gistic CNV_plot 可嵌套进入流程

--- CNV_plot_v2.r
+++ CNV_plot_v2.r
@@ -18,6 +18,7 @@ for (i in pre_info[,2]) {
   count = count + i
 }
 info <- data.frame(pre_info,counts)
+info <- info[!grepl("[A-Za-z]",info[,1]),]  #  这里要求所有的常染色体 都必须是数字!
 ########################################################
 #info <- read.delim("plussize",header=F,sep="\t")
 
@@ -38,24 +39,65 @@ for(i in 1:nrow(dd) ) {
 }
 
 
-my_palette <- t(col2rgb(colorRampPalette(c("blue", "white", "red"))(n = 30)))
-colors<-color.scale(as.numeric(dat$CN),my_palette[,1],my_palette[,2],my_palette[,3]);
-color.legend<-color.gradient(my_palette[,1],my_palette[,2],my_palette[,3],nslices=30);
+my_palette <- t(col2rgb(colorRampPalette(c("blue", "white", "red"))(n = 5)))
+#colors<-color.scale(as.numeric(dat$CN),my_palette[,1],my_palette[,2],my_palette[,3]);
+color.legend<-color.gradient(my_palette[,1],my_palette[,2],my_palette[,3],nslices=31);
+color.legend <- color.legend[-16]
+lev <- seq(-1,1,length.out = 30)
+getcolor <- function(CNV)
+{
+  
+  if(CNV==-1){
+    return(color.legend[1])
+  }else if(CNV == 1){
+      return(color.legend[30])
+  }else if(CNV==0){
+      return("#FFFFFFFF")
+  }
+  for(i in 1:length(lev)-1)
+  {
+    if( CNV>=lev[i] && CNV<=lev[i+1] )
+    {
+      return(color.legend[1+i])
+    }
+  }
+}
+
+colors <- c()
+for(cn in dat$CN){
+  colors=c(colors,getcolor(cn))
+}
+
 dat$color <- colors;
 
+
 len <- (ncol(dd)-4)
+
 sample=colnames(dd)[5:ncol(dd)]
 max = info[nrow(info),2] + info[nrow(info),3]
 per=(max/60)
 chr=info[,1]
 
-##### 调整染色体 防止出不全
+
 chr_pos <- as.numeric((info[,2]/2+info[,3]))/per
-chr_pos[22] <- 60.2
-chr_pos[21] <- 58.9
+##### 如果遇到染色体无法显示的问题,可以考虑调整间隔,放开下方的注释  通过增大JIANGE 变量 显示所有染色体
+# JIANGE = 1.1
+# chr_pos_temp <- rev(chr_pos)
+# for (i in 1:length((chr_pos_temp))) {
+#   if(i==1){
+#     next
+#   }else{
+#       last = i - 1
+#       interval <- chr_pos_temp[last]-chr_pos_temp[i]
+#       if(interval<JIANGE){
+#         chr_pos_temp[i] = chr_pos_temp[i] - (JIANGE-interval)
+#       }
+#     }
+# }
+# chr_pos <- rev(chr_pos_temp)
 ########## 绘制图像 ##############################
 pdf(output,width=16,height=4)
-par(mar=c(4,12,3,4))
+par(mar=c(4,12,3,4),cex.axis = 0.7)
 plot(1:len+2,1:len,type="n",ylim=c(0,len+1),xlim=c(0,60),bty="n",yaxt="n",xaxt="n",xlab="Chromosome",ylab="",xaxs="i");
 axis(2,at=(c(1:len)-0.5),labels=sample,las=2,col = "NA", col.ticks = "NA");
 axis(1,at=(chr_pos),labels=chr,las=1,col = "NA", col.ticks = "NA");
