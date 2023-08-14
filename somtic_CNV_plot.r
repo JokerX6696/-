7990096f3ea05816c6f009fbb0efc1275afae3f1
JokerX6696 <313445042@qq.com>
2023-08-14 14:56:29 +0800

    将somtic 拓展至 常规 cnv,二者兼容

--- somtic_CNV_plot.r
+++ somtic_CNV_plot.r
@@ -1,4 +1,3 @@
-
 # 传参
 library(optparse)
 
@@ -6,7 +5,7 @@ option_list <- list(
   make_option(c("-i","--input_dir"),type="character",help="06.somatic_CNV 目录：推荐使用绝对路径,使用相对路径请确认setwd()是否正确"),
   make_option(c("-g","--genome_sizes"),type="character",help="参考基因组对应的 genome.fa.sizes.chrom ！"),
   make_option(c("-o","--output"),type="character",default='./somatic_CNV_heatmap.pdf',help="输出 pdf 文件名,默认输出到当前路径下")
-  
+
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
@@ -22,12 +21,13 @@ output<- opt$o
 get_df <- function(path){
   col_save <- c("chromosome", "start", "end", "cn")
   files <- list.files(path, full.names = TRUE, recursive = TRUE)
-  files <- files[grepl(pattern = "\\.somatic_CNV\\.xls$",perl = TRUE,x = files)]
+  files <- files[grepl(pattern = "CNV\\.xls$",perl = TRUE,x = files)]
   for (q in files) {
     df_temp <- read.table(file = q, sep = '\t', quote = "",header = TRUE)
     df_temp <- df_temp[,col_save]
-    name_temp <- gsub('.somatic_CNV.xls','',q)
-    name_temp <- gsub('.+/','',name_temp)
+    name_temp <- gsub('.+/','',q)
+    name_temp <- gsub('.somatic_CNV.xls','',name_temp)
+    name_temp <- gsub('\\.CNV.xls','',name_temp)
     df_temp$sample <- name_temp
     if(q == files[1]){
       df <- df_temp
@@ -44,7 +44,7 @@ get_df <- function(path){
     end=df$end,
     CN=df$cn
   )
-  
+
   return(df)
 }
 
@@ -83,7 +83,7 @@ color.legend<-color.gradient(my_palette[,1],my_palette[,2],my_palette[,3],nslice
 lev <- seq(0,4,length.out = 5)
 getcolor <- function(CNV)
 {
-  
+
   if(CNV==0){
     return(color.legend[1])
   }else if(CNV == 1){
@@ -96,7 +96,7 @@ getcolor <- function(CNV)
     print('拷贝数错误,请检查 dat 矩阵 是否有 其他拷贝数 拷贝数只有 0 1 3 4')
     q()
   }
-  
+
 }
 
 colors <- c()
@@ -153,5 +153,3 @@ lines(c(0,0), c(0,len/4), type = 'l')
 lines(c(60,60), c(0,len/4), type = 'l')
 color.legend(53,len/4+0.3,59,len/4+0.5,c(0,1,2,3,'  >=4'),color.legend,gradient="x");
 dev.off()
-############################################################
-
