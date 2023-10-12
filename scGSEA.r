a7a45aaf115f050adcd55dfafdae0445b45f918d
JokerX6696 <313445042@qq.com>
2023-10-12 16:08:42 +0800

    单细胞 GSEA 分析

--- scGSEA.r
+++ scGSEA.r
@@ -2,16 +2,17 @@
 #  单细胞 GSEA 分析
 rm(list=ls())
 setwd("D:/desk/志成单细胞")
-
+library('ggplot2')
 library('Seurat')
 library('msigdbr')
 library('clusterProfiler')
 library('enrichplot')
 all_rds <- c('Epithelium_data_ob_v3.rds','B_cells_data_ob_v3.rds','T_cells_data_ob_v3.rds')
 # 读取 rds
-sc_data <- readRDS('Epithelium_data_ob_v3.rds')
-sc_data <- subset(x = sc_data,sub_celltype=='Goblet_cells')
-cell_type <- 'Goblet_cells'
+cell_type <- 'MAI_T_cells'
+sc_data <- readRDS('T_cells_data_ob_v3.rds')
+sc_data <- subset(x = sc_data,sub_celltype==cell_type)
+
 sx <- sc_data@meta.data
 
 mk_gene <- FindMarkers(object = sc_data,ident.1 = "R_7", ident.2 = "S_7",assay = 'RNA',group.by = 'group')
@@ -29,9 +30,10 @@ dir.create(cell_type)
 setwd(cell_type)
 # 结果保存
 res <-  data.frame(ret)
-write.csv(res,paste0(cell_type,'_GSEA','.csv'))
+write.csv(res,paste0(cell_type,'_GSEA','.csv'),row.names = F)
 #gseaplot(x = ret,geneSetID = 1,title = ret@result$ID[1],by = 'runningScore')
 for (i in seq_along(ret@result$ID)) {
-  gseaplot2(x = ret,geneSetID = i,color="red",pvalue_table = T,title="",base_size=10,ES_geom="line")
+  p <- gseaplot2(x = ret,geneSetID = i,color="red",pvalue_table = F,title=ret@result$ID[[i]],base_size=10,ES_geom="line")
+  ggsave(filename = paste0(cell_type,'-',ret@result$ID[[i]],'.pdf'),plot = p,device = 'pdf',width = 9,height = 6)
 }
 
