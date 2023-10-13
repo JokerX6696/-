6ab575ae60dd4fc0e2092c980128d2a2bcd381d0
JokerX6696 <313445042@qq.com>
2023-10-13 14:32:32 +0800

    单细胞 cellphoneDB 文件准备 并做同源转换

--- pre_cellphoneDB.r
+++ pre_cellphoneDB.r
@@ -57,3 +57,35 @@ newobj_S7 <- merge(x=obj_Goblet_cells_S_7,y=c(
 ))
 
 
+# 提取细胞类型
+meta_R7 <- data.frame(cell = rownames(newobj_R7@meta.data),cell_type=newobj_R7@meta.data$sub_celltype)
+meta_S7 <- data.frame(cell = rownames(newobj_S7@meta.data),cell_type=newobj_S7@meta.data$sub_celltype)
+write.table(meta_R7, file = "R7_celltype.txt", append = FALSE, quote = FALSE, sep = "\t",row.names = FALSE,col.names = TRUE)
+write.table(meta_S7, file = "S7_celltype.txt", append = FALSE, quote = FALSE, sep = "\t",row.names = FALSE,col.names = TRUE)
+# 提取稀疏矩阵
+data_R7=as.data.frame(newobj_R7@assays$RNA@counts)
+data_R7 <- data.frame(Gene=rownames(data_R7),data_R7)
+data_S7=as.data.frame(newobj_S7@assays$RNA@counts)
+data_S7 <- data.frame(Gene=rownames(data_S7),data_S7)
+# 鸡2人
+j2h <- read.table('homologene_in_2_out.xls',header = T)
+
+data_R7 <- data_R7[data_R7$Gene %in% j2h$in_genenames,]
+temp <- c()
+for (i in data_R7$Gene) {
+  ret <- j2h$out_genenames[which(i == j2h$in_genenames)]
+  temp <- c(temp,ret)
+}
+data_R7$Gene <- temp
+
+data_S7 <- data_S7[data_S7$Gene %in% j2h$in_genenames,]
+temp <- c()
+for (i in data_S7$Gene) {
+  ret <- j2h$out_genenames[which(i == j2h$in_genenames)]
+  temp <- c(temp,ret)
+}
+data_S7$Gene <- temp
+
+
+write.table(data_R7, file = "R7_counts.txt", append = FALSE, quote = FALSE, sep = "\t",row.names = F,col.names = TRUE)
+write.table(data_S7, file = "S7_counts.txt", append = FALSE, quote = FALSE, sep = "\t",row.names = F,col.names = TRUE)
